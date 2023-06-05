880d40d25549c7f982697c93867beb1969119804
Carlos <114085990+Anti-404@users.noreply.github.com>
2023-06-05 09:02:42 -0300

    deleting old repository

--- api/src/models/Categories.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace src\models;
-use \core\Model;
-
-class Categories extends Model {
-
-}
--- api/src/models/Things.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace src\models;
-use \core\Model;
-
-class Things extends Model {
-    //Thing = Object 
-    // model has the same name tha table db
-}
